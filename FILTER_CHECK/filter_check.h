7e5399121ae4318dc9ded83a1f7924ca55e24944
Travelerrrrrr <kaixuanguo563@gmail.com>
2026-05-15 19:16:30 +0800

    Update

--- FILTER_CHECK/filter_check.h
+++ FILTER_CHECK/filter_check.h
@@ -22,22 +22,15 @@ typedef enum
     FILTER_BANDSTOP = 5
 } FILTER_TYPE;
 
-typedef enum
-{
-    RFFT_UNIT_RAW = 0, // ADC原始数据
-    RFFT_UNIT_DBV,
-    RFFT_UNIT_DBFS
-} FILTER_UNIT;
-
 typedef struct
 {
     uint16_t pRais;
     uint16_t pFall;
     FILTER_TYPE type;
-} FILTER_INFO;
+} _filter_info;
 
 #if USE_FILTER_CHECK
-FILTER_INFO filter_check(uint32_t addr, uint8_t Msize, uint16_t Length, float threshold, uint8_t BitWidth, float ADC_Ref);
+_filter_info filter_check(uint32_t addr, uint8_t Msize, uint16_t Length, float threshold);
 #endif
 
 #endif // FILTER_CHECK_H
